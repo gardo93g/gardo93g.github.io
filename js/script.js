19cadfdae8389a478d509c990254c586fac1d969
gardo <gardo93g@gmail.com>
2025-11-19 00:35:05 -0600

    apply changes
    Apply website changes

--- js/script.js
+++ js/script.js
@@ -1,62 +1,46 @@
-/*!
-* Start Bootstrap - Resume v7.0.5 (https://startbootstrap.com/theme/resume)
-* Copyright 2013-2022 Start Bootstrap
-* Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-resume/blob/master/LICENSE)
-*/
-//
-// Scripts
-// 
-
-// window.addEventListener('DOMContentLoaded', event => {
-
-//     // Activate Bootstrap scrollspy on the main nav element
-//     const sideNav = document.body.querySelector('#sideNav');
-//     if (sideNav) {
-//         new bootstrap.ScrollSpy(document.body, {
-//             target: '#sideNav',
-//             offset: 74,
-//         });
-//     };
-
-//     // Collapse responsive navbar when toggler is visible
-//     const navbarToggler = document.body.querySelector('.navbar-toggler');
-//     const responsiveNavItems = [].slice.call(
-//         document.querySelectorAll('#navbarResponsive .nav-link')
-//     );
-//     responsiveNavItems.map(function (responsiveNavItem) {
-//         responsiveNavItem.addEventListener('click', () => {
-//             if (window.getComputedStyle(navbarToggler).display !== 'none') {
-//                 navbarToggler.click();
-//             }
-//         });
-//     });
-
-// });
-
-// Owlcarousel
-$(document).ready(function () {
-    $(".owl-carousel").owlCarousel({
+$(document).ready(function(){
+    
+    // Carousel para TreeCloud (Ancho normal, puede mostrar 2)
+    $(".project-carousel").owlCarousel({
         loop: true,
-        margin: 10,
-        nav: true,
+        margin: 15,
+        nav: false,
+        dots: true,
         autoplay: true,
         autoplayTimeout: 6000,
-        autoplayHoverPause: true,
-        center: true,
-        navText: [
-            "<i class='fa fa-angle-left'></i>",
-            "<i class='fa fa-angle-right'></i>"
-        ],
         responsive: {
-            0: {
-                items: 1
-            },
-            600: {
-                items: 1
-            },
-            1000: {
-                items: 2
-            }
+            0: { items: 1 },
+            600: { items: 2 },
+            1000: { items: 2 }
         }
     });
+
+    // Carousel para Kubiak (Columnas divididas) y otros de un solo item
+    $(".single-item-carousel").owlCarousel({
+        loop: true,
+        margin: 10,
+        nav: false,
+        dots: true,
+        items: 1,
+        autoplay: true,
+        autoplayTimeout: 5000
+    });
+
+    // Navbar shrink effect
+    $(window).scroll(function() {
+        if ($(document).scrollTop() > 50) {
+            $('.navbar').addClass('shrink');
+        } else {
+            $('.navbar').removeClass('shrink');
+        }
+    });
+
+    // Video Hover Effect (Optional, if you prefer hover over autoplay)
+    /*
+    $(".project-card").hover(function(){
+        $(this).find("video").get(0).play();
+    }, function(){
+        $(this).find("video").get(0).pause();
+    });
+    */
 });
